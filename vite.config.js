9770cf9e5877389a51c1c2af09b807399a70dc0a
wandageijoshua <wandageijoshua@gmail.com>
2026-03-14 10:56:19 +0300

    all the current changes this mornongJosh

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'es2015',
-    minify: 'terser',
+    target: "es2015",
+    minify: "terser",
     terserOptions: {
       compress: {
         drop_console: true,
         drop_debugger: true
       }
-    },
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id
-              .toString()
-              .split('node_modules/')[1]
-              .split('/')[0]
-              .toString();
-          }
-        }
-      }
     }
-  },
-  css: {
-    devSourcemap: false
   }
 });
